d761f857a45545b6dc0f7ffe0ad790c7735a2534
Joseph Peng <Joseph@StacydeMacBook-Air-2.local>
2026-01-30 22:59:37 -0500

    User requested commit

--- hw3-jjpeng0308/HW3.py
+++ hw3-jjpeng0308/HW3.py
@@ -3,7 +3,7 @@
 # Email: jjpeng@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
-# e.g.:
+# e.g.: One thing I got help is still figuring out how to use the terminal and git commit. and some other helps I got is checking why isn't it working.
 # Asked ChatGPT hints for debugging and suggesting the general structure of the code
 # Did your use of GenAI on this assignment align with your goals and guidelines in 
 #    your Gen AI contract? If not, why?
